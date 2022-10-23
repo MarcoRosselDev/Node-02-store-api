a87ceea1446cac6493dcb70186672367de289fa6
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-23 04:41:19 -0300

    bug in usecreateindex and usefindandmodify not more suport

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 require("dotenv").config();
-// async errors
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
-const { log } = require("console");
 
 // middleware
 app.use(express.json());
 
-// rootes
+// routes
 
 app.get("/", (req, res) => {
-  res.send('<h1>Store API</h1><a href="/api/v1/products">products</a>');
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+
+app.use("/api/v1/products", productsRouter);
 
 // products route
 
@@ -30,7 +32,7 @@ const start = async () => {
   try {
     // connectDB
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`Server is listening port ${port}...`));
+    app.listen(port, () => console.log(`Server is listening port ${port}...`));
   } catch (error) {
     console.log(error);
   }
