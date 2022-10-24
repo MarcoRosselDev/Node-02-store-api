8a8ef7fa85c7ed76750dfc2f46bd63d588df8be8
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-24 06:07:31 -0300

    populete.js

--- populete.js
+++ populete.js
@@ -1,4 +1,15 @@
 require("dotenv").config();
 
+const connectDB = require("./db/connect");
 const connetDB = require("./db/connect");
 const Product = require("./models/product");
+
+const jsonProducts = require("./products.json");
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+  } catch (error) {
+    console.log(error);
+  }
+};
