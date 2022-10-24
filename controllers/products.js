4e27b1c5cf6bb58fc357f44185dd0e24a2345635
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-24 08:33:35 -0300

    req.query object return

--- controllers/products.js
+++ controllers/products.js
@@ -5,7 +5,14 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
 // {{URL}}/products?name=john&featured=true on postman then log= { name: 'john', featured: 'true' }
