07a6bc0d9b3559e0e000df56f98f46b7380096da
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-24 05:10:04 -0300

    mongoose structure

---
+++ models/product.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "product name must be provided"] },
+  price: { type: Number, required: [true, "product price must be provided"] },
+  featured: { type: Boolean, default: false },
+});
