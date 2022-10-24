47382dc223718fabe1c7fde82b4cf963cd28eea7
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-24 09:13:24 -0300

    solving not request exactli name array

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,21 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ name: "vase table" });
+  const products = await Product.find({ name: "albany" });
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = name;
   }
   console.log(queryObject);
   const products = await Product.find(queryObject);
