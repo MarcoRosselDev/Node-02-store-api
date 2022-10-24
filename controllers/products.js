1665829c5bdd949621c24d92eeba71b9fe442c1a
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-24 08:19:34 -0300

    same logic on getallproducts

--- controllers/products.js
+++ controllers/products.js
@@ -5,8 +5,8 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  console.log(req.query);
-  res.status(200).json({ msg: "products route" });
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 // {{URL}}/products?name=john&featured=true on postman then log= { name: 'john', featured: 'true' }
 
